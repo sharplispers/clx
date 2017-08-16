7350db386c98abf29df521feb292358f03969c60
Tomek Kurcz <tomsandbox@gmail.com>
2017-08-16 09:12:53 +0200

    Delete obsolete code in tests/test.lisp

--- tests/test.lisp
+++ tests/test.lisp
@@ -8,14 +8,3 @@
 
 (fiasco:defsuite (xlib-all-tests :bind-to-package :xlib-test))
 
-(defvar *test-suites* nil)
-
-(defun run-all-tests (&rest fiasco-params)
-  (apply #'fiasco:run-package-tests :packages *test-suites* fiasco-params))
-
-(defmacro define-test-suite (name &body package-options)
-  `(progn (fiasco:define-test-package (,name :in xlib-all-tests)
-            ,@package-options
-            (:use :xlib-test))
-          (in-package ,name)
-          (setf *test-suites* (adjoin ,name *test-suites*))))
