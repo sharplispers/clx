89eed9278e3c9c966de1aa0cb6467c547756c8c3
Luís Oliveira <luismbo@gmail.com>
2018-01-31 18:59:27 +0000

    ci-doc-gh-pages.sh: update repository link

--- ci-doc-gh-pages.sh
+++ ci-doc-gh-pages.sh
@@ -23,7 +23,7 @@ git init
 git config user.name "Documentation Bot"
 git config user.email "Bender@future.com"
 
-git remote add upstream "https://$GH_TOKEN@github.com/PuercoPop/clx-manual-ci-test.git"
+git remote add upstream "https://$GH_TOKEN@github.com/sharplispers/clx.git"
 git fetch upstream
 git reset upstream/gh-pages
 
